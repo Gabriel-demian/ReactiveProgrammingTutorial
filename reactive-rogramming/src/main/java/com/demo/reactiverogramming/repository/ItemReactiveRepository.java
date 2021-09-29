9c3b851add7bc733d9ca803ea29e3434bc9fd896
Gabriel <57782295+Gabriel-demian@users.noreply.github.com>
2021-09-29 01:32:04 -0300

    ItemReactiveRepositoryTest
    ItemReactiveRepositoryTest
    ->saveItemTest
    ->updateItemTest
    ->deleteItemByIdTest
    ->deleteItemByIdTest2

--- reactive-rogramming/src/main/java/com/demo/reactiverogramming/repository/ItemReactiveRepository.java
+++ reactive-rogramming/src/main/java/com/demo/reactiverogramming/repository/ItemReactiveRepository.java
@@ -2,9 +2,10 @@ package com.demo.reactiverogramming.repository;
 
 import com.demo.reactiverogramming.document.Item;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface ItemReactiveRepository extends ReactiveMongoRepository<Item, String> {
 
-    Flux<Item> findByDescription(String description);
+    Mono<Item> findByDescription(String description); // since it is returning only one result we don't need a Flux. With Mono is enough
+
 }
--- reactive-rogramming/src/test/java/com/demo/reactiverogramming/reporitory/ItemReactiveRepositoryTest.java
+++ reactive-rogramming/src/test/java/com/demo/reactiverogramming/reporitory/ItemReactiveRepositoryTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import java.util.Arrays;
@@ -39,12 +40,12 @@ public class ItemReactiveRepositoryTest {
                 .doOnNext((item -> {
                     System.out.println("Inserted Item is: " + item);
                 }))
-                .blockLast(); // espera a que todos los archivos esten guardados.
+                .blockLast(); // wait until all files are saved.
 
     }
 
     @Test
-    public void getAllItems(){
+    public void getAllItemsTest(){
         StepVerifier.create(itemReactiveRepository.findAll())
                 .expectSubscription()
                 .expectNextCount(5)
@@ -52,7 +53,7 @@ public class ItemReactiveRepositoryTest {
     }
 
     @Test
-    public void getItemById(){
+    public void getItemByIdTest(){
         StepVerifier.create(itemReactiveRepository.findById("123"))
                 .expectSubscription()
                 .expectNextMatches((item) -> item.getDescription().equals("Samsung Watch"))
@@ -60,7 +61,7 @@ public class ItemReactiveRepositoryTest {
     }
 
     @Test
-    public void findItemByDescription(){
+    public void findItemByDescriptionTest(){
         StepVerifier.create(itemReactiveRepository.findByDescription("Samsung Watch"))
                 .expectSubscription()
                 .expectNextMatches((item) -> item.getDescription().equals("Samsung Watch") && item.getId().equals("123"))
@@ -72,6 +73,70 @@ public class ItemReactiveRepositoryTest {
                 .verifyComplete();
     }
 
+    @Test
+    public void saveItemTest(){
+        // cuando guardamos un item de manera no bloqueante obtenemos un Mono.
+        Item item = new Item(null, "Google Home Mini", 20.00);
+        Mono<Item> savedItem = itemReactiveRepository.save(item);
 
+        StepVerifier.create(savedItem)
+                .expectSubscription()
+                .expectNextMatches(item1 -> item1.getId()!=null && item1.getDescription().equals("Google Home Mini") && item1.getPrice() == 20.00)
+                .verifyComplete();
+    }
 
+    @Test
+    public void updateItemTest(){
+
+        double newPrice = 520.00;
+        Mono<Item> updatedItem = itemReactiveRepository.findByDescription("LG TV")
+                .map(item -> {
+                    item.setPrice(newPrice); // setting the new price
+                    return item;
+                })
+                .flatMap(item -> {
+                    return itemReactiveRepository.save(item); // saving the item with the new price
+                });
+
+        StepVerifier.create(updatedItem)
+                .expectSubscription()
+                .expectNextMatches(item -> item.getPrice()==520.00)
+                .verifyComplete();
+    }
+
+    @Test
+    public void deleteItemByIdTest(){
+
+        Mono<Void> deletedItem = itemReactiveRepository.findById("123") // Mono<Item>
+                .map(Item::getId) // get id -> transform from one type to another type
+                .flatMap((id) -> {
+                    return itemReactiveRepository.deleteById(id);
+                });
+
+        StepVerifier.create(deletedItem)
+                .expectSubscription()
+                .verifyComplete();
+
+        StepVerifier.create(itemReactiveRepository.findAll().log("The new Item List: "))
+                .expectNextCount(4)
+                .verifyComplete();
+    }
+
+    @Test
+    public void deleteItemByIdTest2(){
+
+        Mono<Void> deletedItem = itemReactiveRepository.findByDescription("LG TV")
+                .map(Item::getId)
+                .flatMap((id) -> {
+                    return itemReactiveRepository.deleteById(id);
+                });
+
+        StepVerifier.create(deletedItem)
+                .expectSubscription()
+                .verifyComplete();
+
+        StepVerifier.create(itemReactiveRepository.findAll().log("The new Item List: "))
+                .expectNextCount(4)
+                .verifyComplete();
+    }
 }
